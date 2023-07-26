7d912e23f38a19f3454e0e8e896e021e32a2fa44
juliana <julianapereira.dev@gmail.com>
2023-07-26 11:01:54 -0300

    Feat: GET for route /customers/:id is now working

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -54,8 +54,25 @@ export async function postCustomer(req, res) {
 
 }
 
-export async function getCustomerId(req, res){
-//
+export async function getCustomerId(req, res) {
+  const {id} = req.params;
+
+  try {
+
+    const idExistQuery = await db.query(`SELECT * FROM customers WHERE id = $1;`, [id]);
+      
+    if (idExistQuery.rows.length === 0) {
+      return res.status(404).send("Este id não existe no banco de clientes");
+    }
+
+    const customerId = await db.query(`
+    SELECT * FROM customers WHERE id=$1;`, [id]
+    )
+    res.send(customerId.rows[0]);
+    
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
 }
 
 export async function updateCustomer(req, res){
