f4b90c706a3d25d34e4bb3552b681191b7139bc7
juliana <julianapereira.dev@gmail.com>
2023-07-26 13:59:47 -0300

    Feat: Installing @joi/date dependencie and try to fix birthday error

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -1,47 +1,47 @@
 import { db } from "../database/database.connection.js";
 
 // Função para converter uma data para o formato 'YYYY-MM-DD'
-function formatDate(date) {
-    const year = date.getUTCFullYear();
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-    const day = String(date.getUTCDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
-
-
-export async function getCustomers(req, res){
-
-    try {
-        const customers = await db.query(`SELECT * FROM customers;`);
-
-// Formatando o birthday antes de enviar como resposta
-    const formattedCustomers = customers.rows.map(customer => {
-        return {
-          ...customer,
-          birthday: formatDate(customer.birthday),
-        };
-      });
-      res.send(formattedCustomers);
-
-      } catch (err) {
-        return res.status(500).send(err.message)
-      }
-
-}
+// function formatDate(date) {
+//     const year = date.getUTCFullYear();
+//     const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+//     const day = String(date.getUTCDate()).padStart(2, '0');
+//     return `${year}-${month}-${day}`;
+//   }
 
 
 // export async function getCustomers(req, res){
 
-//   try {
-//       const customers = await db.query(`SELECT * FROM customers;`);
+//     try {
+//         const customers = await db.query(`SELECT * FROM customers;`);
 
-//     res.send(customers.rows);
+// // Formatando o birthday antes de enviar como resposta
+//     const formattedCustomers = customers.rows.map(customer => {
+//         return {
+//           ...customer,
+//           birthday: formatDate(customer.birthday),
+//         };
+//       });
+//       res.send(formattedCustomers);
 
-//     } catch (err) {
-//       return res.status(500).send(err.message)
-//     }
+//       } catch (err) {
+//         return res.status(500).send(err.message)
+//       }
 
 // }
+
+
+export async function getCustomers(req, res){
+
+  try {
+      const customers = await db.query(`SELECT * FROM customers;`);
+
+    res.send(customers.rows);
+
+    } catch (err) {
+      return res.status(500).send(err.message)
+    }
+
+}
 
 export async function postCustomer(req, res) {
 
@@ -53,12 +53,10 @@ export async function postCustomer(req, res) {
       if (cpfExistQuery.rows.length > 0) {
         return res.status(409).send("Este cpf já existe no banco de clientes");
       }
-    
-      const formattedBirthday = formatDate(new Date(birthday)); // Format the birthday here
 
       await db.query(
         `INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)`,
-        [name, phone, cpf, formattedBirthday] // Use the formattedBirthday variable here
+        [name, phone, cpf, birthday]
       );
      res.sendStatus(201)
     
--- src/schemas/customers.schemas.js
+++ src/schemas/customers.schemas.js
@@ -1,8 +1,11 @@
-import joi from 'joi';
+import baseJoi from 'joi';
+import joiDate from "@joi/date";
+
+const joi = baseJoi.extend(joiDate) 
 
 export const customerSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().min(10).max(11).pattern(/^[0-9]+$/).required(),
     cpf: joi.string().length(11).pattern(/^[0-9]+$/).required(),
-    birthday: joi.date().max('now').iso(),
+    birthday: joi.date().format("YYYY-MM-DD")
 });
