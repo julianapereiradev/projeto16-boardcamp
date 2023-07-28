2027eb6e2d96183e8bc10ff18c335c97c7861dac
juliana <julianapereira.dev@gmail.com>
2023-07-28 12:05:25 -0300

    Build: Settings for Deploy

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.routes.js";
+import dotenv from "dotenv"
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(router)
+app.use(router);
+dotenv.config()
 
 //PORT:
-const PORT = 5000;
-app.listen(PORT, () =>
-  console.log(`O servidor está rodando na porta ${PORT}!`)
-);
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
