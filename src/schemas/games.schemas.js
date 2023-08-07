d3d747699393444204c65065489b9b2d64c0f17c
juliana <julianapereira.dev@gmail.com>
2023-08-07 11:00:25 -0300

    Adding comments

--- src/schemas/games.schemas.js
+++ src/schemas/games.schemas.js
@@ -4,5 +4,5 @@ export const gameSchema = joi.object({
     name: joi.string().required(),
     stockTotal: joi.number().positive().required(),
     pricePerDay: joi.number().positive().required(),
-    image: joi.required(),
+    image: joi.required(), //faltou .uri()
 });
